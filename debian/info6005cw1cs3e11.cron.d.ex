1bd3fbbc085ff2ce37b04feccef9993415019534
Chrystalla <cs3e11@soton.ac.uk>
2012-02-23 19:23:15 +0200

    Adding debian package files

---
+++ debian/info6005cw1cs3e11.cron.d.ex
@@ -0,0 +1,4 @@
+#
+# Regular cron jobs for the info6005cw1cs3e11 package
+#
+0 4	* * *	root	[ -x /usr/bin/info6005cw1cs3e11_maintenance ] && /usr/bin/info6005cw1cs3e11_maintenance
---
+++ debian/info6005cw1cs3e11.default.ex
@@ -0,0 +1,10 @@
+# Defaults for info6005cw1cs3e11 initscript
+# sourced by /etc/init.d/info6005cw1cs3e11
+# installed at /etc/default/info6005cw1cs3e11 by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
